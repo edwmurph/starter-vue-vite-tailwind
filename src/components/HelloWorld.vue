0350619bd9479c4d96a772162dd556b9bef7954d
edwmurph <edward@edwardmurphy.dev>
2022-02-26 17:22:51 -0500

    lint fixes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,11 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from 'vue';
 
 defineProps({
   msg: String
-})
+});
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
@@ -32,6 +32,7 @@ const count = ref(0)
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
 </template>
+
 
 <style scoped>
 a {
