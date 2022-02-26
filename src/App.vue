809e79112bc6e24da228978a12884046cc0ffe91
edwmurph <edward@edwardmurphy.dev>
2022-02-26 17:26:03 -0500

    add tailwind

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@ import HelloWorld from './components/HelloWorld.vue';
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
